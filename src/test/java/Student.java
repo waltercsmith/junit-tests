47109fbf0e953d766e46e0dcf22644cc3149c9ea
Walter Smith <walter.c.smith01@gmail.com>
2021-02-22 15:08:49 -0500

    Created a contructor that sets both the name and id property

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,9 +1,42 @@
 import java.util.ArrayList;
 
 public class Student {
+
+
+//    constructor that sets both the name and id property
+    public Student(String studentName, long studentId) {
+        this.studentName = studentName;
+        this.studentId = studentId;
+    }
+
+
+
+
+    // returns the student's name
+    public String getStudentName() {
+        return studentName;
+    }
+
     String studentName;
+
+
+    // returns the student's id
+    public long getStudentId() {
+        return studentId;
+    }
+
     long studentId;
+
+
+    // returns the list of grades
+
+    public ArrayList<Integer> getStudentGrades() {
+        return studentGrades;
+    }
+
     ArrayList<Integer> studentGrades;
+
+
 
 }
 
